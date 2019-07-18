4c82596722d90415725d02118620db6401916ed4
witty2454 <51719341+witty2454@users.noreply.github.com>
2019-07-18 13:28:10 -0500

    uploading my stack and queue classes

---
+++ SweepstakesQueueManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sweepstakes
+{
+    public class SweepstakesQueueManager : ISweepstakesManager
+    {
+        Queue<Sweepstakes> theQueue;
+
+        public SweepstakesQueueManager()
+        {
+            theQueue = new Queue<Sweepstakes>();
+        }
+
+        public Sweepstakes GetSweepstakes()
+        {
+            return theQueue.Dequeue();
+        }
+
+        public void InsertSweepstakes(Sweepstakes sweepstakes)
+        {
+            theQueue.Enqueue(sweepstakes);
+        }
+    }
+}
---
+++ SweepstakesStackManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sweepstakes
+{
+    public class SweepstakesStackManager : ISweepstakesManager
+    {
+        public Stack<Sweepstakes> theStack;
+
+        public SweepstakesStackManager()
+        {
+            theStack = new Stack<Sweepstakes>();
+        }
+
+        public Sweepstakes GetSweepstakes()
+        {
+            return theStack.Pop();
+        }
+
+        public void InsertSweepstakes(Sweepstakes sweepstakes)
+        {
+            theStack.Push(sweepstakes);
+        }
+    }
+}
