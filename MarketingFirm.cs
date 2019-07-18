cbd7408cbac7033bb0464e646ea3f9867eba2346
witty2454 <51719341+witty2454@users.noreply.github.com>
2019-07-18 14:27:55 -0500

    added my marketing class

---
+++ MarketingFirm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sweepstakes
+{
+    public class MarketingFirm
+    {
+        public Sweepstakes sweepstakes;
+        private string managementOption;
+        ISweepstakesManager manager;
+        Contestant contestant;
+
+        public void ManagerFactory()
+        {
+            managementOption = UserInterface.GetStackOrQueueSelection();            
+            switch (managementOption)
+            {
+                case "stack":
+                    manager = new SweepstakesStackManager();
+                    break;
+                case "queue":
+                    manager = new SweepstakesQueueManager();
+                    break;
+                default:
+                    break;
+            }
+            SetUpSweepstakes(manager);
+        }
+
+        public void SetUpSweepstakes(ISweepstakesManager manager)
+        {
+            sweepstakes = new Sweepstakes(UserInterface.GetSweepstakesName());            
+            do
+            {
+                contestant = new Contestant();
+                sweepstakes.RegisterContestant(contestant);
+            } while (UserInterface.GetMoreContestants() == "yes");
+            manager.InsertSweepstakes(sweepstakes);
+            MakeMoreSweepstakes();            
+        }
+
+        public void MakeMoreSweepstakes()
+        {
+            if (UserInterface.GetMoreSweepstakes() == "yes")
+            {
+                SetUpSweepstakes(manager);
+            }
+            else
+            {
+                UserInterface.DisplayOkThenWinners();
+                GetOneWinner();
+            }
+        }
+        public void GetOneWinner()
+        {            
+            Contestant winner = manager.GetSweepstakes().PickWinner();
+            UserInterface.DisplayWinnerMessage();
+            sweepstakes.PrintContestantInfo(winner);
+            if (UserInterface.GetMoreWinners() == "yes")
+            {
+                GetOneWinner();
+            }
+            else
+            {
+                UserInterface.DisplayOkThenMoreSweepstakes();
+                MakeMoreSweepstakes();
+            }
+        }
+    }
+}
